2740a7830800d72e006bc048f11cef62971ab4fa
Chinmoy R. Pradhan <chinmoyrp65@gmail.com>
2023-12-14 02:56:12 +0530

    Use customasm for program gen and remove asm block

---
+++ soc/fib.asm
@@ -0,0 +1,31 @@
+#bankdef mybank {
+  #outp 0x0
+  #bits 16
+}
+
+#bank mybank
+
+#subruledef reg {
+    r0 => 0
+    r1 => 1
+    r2 => 2
+    r3 => 3
+}
+
+#ruledef {
+    add {rd:reg}, {value}   => value`8  @ 0`3   @ 0`2   @ rd`2  @ 0`1
+    add {rd:reg}, {rs:reg}  => 0`8      @ 0`3   @ rs`2  @ rd`2  @ 1`1
+    move {rd:reg}, {value}  => value`8  @ 6`3   @ 0`2   @ rd`2  @ 0`1
+    move {rd:reg}, {rs:reg} => 0`8      @ 6`3   @ rs`2  @ rd`2  @ 1`1
+    jump {value}            => value`8  @ 7`3   @ 0`2   @ 0`2   @ 0`1
+    jump {rs:reg}           => 0`8      @ 7`3   @ rs`2  @ 0`2   @ 1`1
+}
+
+fibonacci:
+    add r2, 1
+    .loop:
+        add r2, r1
+        move r3, r2
+        add r1, r2
+        move r3, r1
+        jump .loop
